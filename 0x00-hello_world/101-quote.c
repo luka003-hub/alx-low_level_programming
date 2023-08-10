a7a604b73404c7d1aae8272f49e99ec95fb33167
luka003-hub <kimathiluka03@gmail.com>
2023-08-10 12:36:49 -0700

    task8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
-
+#include <unistd.h>
 /**
- * main - Entry point
- * Return: Always 1 (Success)
+ * main - A c program thst ptints a line to the standard error
+ * Return 1 (success)
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
+	fput("and that piece of art is useful\\" - Dora Kor par, 2015-10-19\n-, stdout);
 	return (1);
 }
