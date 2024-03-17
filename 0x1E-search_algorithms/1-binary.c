a39fda62c4902f4e7ad05aaab6379d460431e534
luka003-hub <kimathiluka03@gmail.com>
2024-03-17 02:21:06 -0700

    tsk2

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,56 +1,37 @@
 #include "search_algos.h"
 
-void print_array(int *array, size_t i, size_t size);
-
 /**
- * binary_search - binary search algorithm in a string
- * @array: list of elements
- * @size: size of the list
- * @value: target to search
- * Return: element index on success (-1) on fail
- */
+  * binary_search - Searches for a value in a sorted array
+  *                 of integers using binary search algorithm
+  * @array: pointer to the first element of the array to search.
+  * @size: number of elements in the array.
+  * @value: value to search for.
+  *
+  * Return: If the value is not present or the array is NULL, -1.
+  *         Otherwise, the index where the value is located
+  */
 int binary_search(int *array, size_t size, int value)
 {
-	size_t low = 0, high = size - 1, half = 0;
+	size_t n, left, right;
 
-	if (array == NULL || value == 0)
+	if (array == NULL)
 		return (-1);
 
-	print_array(array, low, size);
-	while (low < high)
+	for (left = 0, right = size - 1; right >= left;)
 	{
-		half = (low + high) / 2;
-		if (array[half] < value)
-		{
-			low = half + 1;
-			print_array(array, low, high + 1);
-		}
-		else if (array[half] > value)
-		{
-			high = half - 1;
-			print_array(array, low, high + 1);
-		}
-		else
-			return (array[half]);
-	}
-	return (-1);
-}
+		printf("Searching in array: ");
+		for (n = left; n < right; n++)
+			printf("%d, ", array[n]);
+		printf("%d\n", array[n]);
 
-/**
- * print_array - print all elements in an array
- * @array: list of elements
- * @i: index to init
- * @size: size of the array
- */
-void print_array(int *array, size_t i, size_t size)
-{
-	printf("Searching in array: ");
-	while (i < size)
-	{
-		if (i + 1 == size)
-			printf("%d ", array[i]);
+		n = left + (right - left) / 2;
+		if (array[n] == value)
+			return (n);
+		if (array[n] > value)
+			right = n - 1;
 		else
-			printf("%d, ", array[i]);
-		i++;
-	} printf("\n");
+			left = n + 1;
+	}
+
+	return (-1);
 }
